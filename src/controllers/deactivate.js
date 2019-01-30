fca090fc890b59fa1b93a1e4a37f4204e66f6a82
Andrew Viteri <aryn.viteri@gmail.com>
2019-01-29 22:38:49 -0500

    Outline delete user feature.

---
+++ src/controllers/deactivate.js
@@ -0,0 +1,14 @@
+/**
+ * FOOTING.
+ * Namespace: src/controllers
+ * January 29, 2019
+ * LICENSE: MIT
+ * Andrew Viteri
+ */
+
+const User = require('../models/user.js');
+const JSONResponse = require('../models/JSONResponse.js');
+
+module.exports = function(config) {
+	
+}
