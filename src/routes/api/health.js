e2ade9d594dfff3003655b4cfb172a04247de901
Andrew Viteri <aryn.viteri@gmail.com>
2019-03-12 19:02:25 -0400

    Refactor health.js

--- src/routes/api/health.js
+++ src/routes/api/health.js
@@ -12,34 +12,24 @@ module.exports = function(config, app, routes) {
 
 	const RequestAuthenticator = require('../middleware/auth.js')(config);
 
-	///////////////////////////////////////////////
-	////     PUBLIC TEST ROUTE - W/O CSRUF    ////
-	/////////////////////////////////////////////
 
+	/* Public test route - w/o CSURF */
 	routes.unprotected.get('/status', function(req, res) {
 		return res.status(200).json({"200":"OK."});
 	});
 
-	///////////////////////////////////////////////
-	/////     PUBLIC TEST ROUTE - W CSRUF    /////
-	/////////////////////////////////////////////
-
+	/* Public test route - w/ CSURF */
 	routes.protected.post('/test/csrf', function(req, res) {
 		return res.status(200).json({"200":"OK."});
 	});
 
-	///////////////////////////////////////////////
-	/////   PRIVATE TEST ROUTE - W/O CSRUF   /////
-	/////////////////////////////////////////////
-
+	/* Private test route - w/o CSURF */
 	routes.unprotected.post('/test/auth', RequestAuthenticator, function(req, res) {
 		return res.status(200).json({"200":"Authenticated."});
 	});
 
-	///////////////////////////////////////////////
-	/////   PRIVATE TEST ROUTE - W CSRUF     /////
-	/////////////////////////////////////////////
-
+	
+	/* Private test route - w CSURF */
 	routes.protected.post('/test/auth_csrf', RequestAuthenticator, function(req, res) {
 		return res.status(200).json({"200":"Authenticated."});
 	});
