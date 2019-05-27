9db55f1daa9575b5c326b035f2d37a56253fac3b
Andrew Viteri <aryn.viteri@gmail.com>
2019-05-27 18:29:34 -0400

    Fix windows commands

--- src/commands.js
+++ src/commands.js
@@ -15,16 +15,16 @@ module.exports = function(_dir, projType, projName) {
 			"installModules": `cd ${projName} && npm install`
 		},
 		"windows": {
-			"createDirs": `mkdir ${projName} ${projName}/src ${projName}/src/config ${projName}/src/controllers ${projName}/src/handlers ${projName}/src/models ${projName}/src/routes ${projName}/src/routes/api ${projName}/src/routes/middleware`,
-			"cpPackageJSON": `copy ${_dir}/lib/package.json ${projName}/package.json`,
-			"cpEnvFile": `copy ${_dir}/lib/env.${projType}.dist ${projName}/.env`,
-			"cpTests": `xcopy ${_dir}/lib/test.${projType} ${projName}/test`,
-			"cpMainFile": `copy ${_dir}/lib/main.${projType}.js ${projName}/src/main.js`,
-			"cpConfigFiles": `copy ${_dir}/lib/config/app.js ${projName}/src/config/app.js && copy ${_dir}/lib/config/routes.js ${projName}/src/config/routes.js && copy ${_dir}/lib/config/server.js ${projName}/src/config/server.js && copy ${_dir}/lib/config/config.${projType}.js ${projName}/src/config/config.js`,
-			"cpControllers": `copy ${_dir}/lib/controllers/user_controller.${projType}.js ${projName}/src/controllers/user_controller.js`,
-			"cpHandlers": `copy ${_dir}/lib/handlers/auth_handler.js ${projName}/src/handlers/auth_handler.js`,
-			"cpModels": `copy ${_dir}/lib/models/errors.js ${projName}/src/models/errors.js && copy ${_dir}/lib/models/user.js ${projName}/src/models/user.js`,
-			"cpRoutes": `copy ${_dir}/lib/routes/api/status_routes.js ${projName}/src/routes/api/status_routes.js && copy ${_dir}/lib/routes/api/user_routes.js ${projName}/src/routes/api/user_routes.js && copy ${_dir}/lib/routes/middleware/auth_middleware.js ${projName}/src/routes/middleware/auth_middleware.js && copy ${_dir}/lib/routes/middleware/csrf_middleware.js ${projName}/src/routes/middleware/csrf_middleware.js`,
+			"createDirs": `mkdir ${projName} ${projName}\\src ${projName}\\src\\config ${projName}\\src\\controllers ${projName}\\src\\handlers ${projName}\\src\\models ${projName}\\src\\routes ${projName}\\src\\routes\\api ${projName}\\src\\routes\\middleware`,
+			"cpPackageJSON": `copy ${_dir}\\lib\\package.json ${projName}\\package.json`,
+			"cpEnvFile": `copy ${_dir}\\lib\\env.${projType}.dist ${projName}\\.env`,
+			"cpTests": `xcopy ${_dir}\\lib\\test.${projType} ${projName}\\test`,
+			"cpMainFile": `copy ${_dir}\\lib\\main.${projType}.js ${projName}\\src\\main.js`,
+			"cpConfigFiles": `copy ${_dir}\\lib\\config\\app.js ${projName}\\src\\config\\app.js && copy ${_dir}\\lib\\config\\routes.js ${projName}\\src\\config\\routes.js && copy ${_dir}\\lib\\config\\server.js ${projName}\\src\\config\\server.js && copy ${_dir}\\lib\\config\\config.${projType}.js ${projName}\\src\\config\\config.js`,
+			"cpControllers": `copy ${_dir}\\lib\\controllers\\user_controller.${projType}.js ${projName}\\src\\controllers\\user_controller.js`,
+			"cpHandlers": `copy ${_dir}\\lib\\handlers\\auth_handler.js ${projName}\\src\\handlers\\auth_handler.js`,
+			"cpModels": `copy ${_dir}\\lib\\models\\errors.js ${projName}\\src\\models\\errors.js && copy ${_dir}\\lib\\models\\user.js ${projName}\\src\\models\\user.js`,
+			"cpRoutes": `copy ${_dir}\\lib\\routes\\api\\status_routes.js ${projName}\\src\\routes\\api\\status_routes.js && copy ${_dir}\\lib\\routes\\api\\user_routes.js ${projName}\\src\\routes\\api\\user_routes.js && copy ${_dir}\\lib\\routes\\middleware\\auth_middleware.js ${projName}\\src\\routes\\middleware\\auth_middleware.js && copy ${_dir}\\lib\\routes\\middleware\\csrf_middleware.js ${projName}\\src\\routes\\middleware\\csrf_middleware.js`,
 			"installModules": `cd ${projName} && npm install`
 		}
 	}
